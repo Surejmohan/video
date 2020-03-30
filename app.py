8981ec83d7e0f20a3b4826e9fc63888b2133ae3b
Surej Mohan <soorajmohan121997@gmail.com>
2020-03-30 18:01:55 +0530

    lockdown

---
+++ app.py
@@ -0,0 +1,223 @@
+from flask import Flask, render_template,request,redirect,url_for
+from werkzeug.utils import secure_filename
+import dlib,cv2
+import numpy as np
+import os
+
+
+UPLOAD_FOLDER = './uploads'
+UPLOAD_VIDEO = './video'
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_VIDEO'] = UPLOAD_VIDEO
+
+detector = dlib.get_frontal_face_detector()
+sp = dlib.shape_predictor('models/shape_predictor_68_face_landmarks.dat')
+facerec = dlib.face_recognition_model_v1('models/dlib_face_recognition_resnet_model_v1.dat')
+
+
+
+def find_faces(image,name):
+
+    dets = detector(image, 1)
+
+    if len(dets) == 0:
+        return np.empty(0), np.empty(0), np.empty(0)
+    if len(dets) > 1:
+         print("Please change image: " + name + " - it has " + str(len(dets)) + " faces; it can only have one")
+         exit()
+
+    
+    rects, shapes = [], []
+    shapes_np = np.zeros((len(dets), 68, 2), dtype=np.int)
+    for k, d in enumerate(dets):
+        rect = ((d.left(), d.top()), (d.right(), d.bottom()))
+        rects.append(rect)
+
+        shape = sp(image, d)
+        
+        # convert dlib shape to numpy array
+        for i in range(0, 68):
+            shapes_np[k][i] = (shape.part(i).x, shape.part(i).y)
+
+        shapes.append(shape)
+        
+    return rects, shapes, shapes_np
+
+
+
+
+
+def encode_faces(img, shapes):
+    face_descriptors = []
+    for shape in shapes:
+        face_descriptor = facerec.compute_face_descriptor(img, shape)
+        face_descriptors.append(np.array(face_descriptor))
+
+    return np.array(face_descriptors)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/current')
+def current():
+    return render_template('current.html')
+
+
+
+
+@app.route('/upload', methods=['POST','GET'])
+
+
+def train():
+    filelist = [f for f in os.listdir('uploads/')]
+    for f in filelist:
+        os.remove(os.path.join('uploads/', f))
+    
+    
+    if request.method == 'POST':
+        
+        uploaded_files = request.files.getlist("file")
+        for f in uploaded_files:
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+        myimages = []
+        dirfiles = os.listdir('uploads/')
+        sorted(dirfiles)
+        for files in dirfiles:
+            if '.jpg' in files:
+                myimages.append(files)
+            if '.jpeg' in files:
+                myimages.append(files)
+        no_of_images = len(myimages)
+        if no_of_images > 2:
+            print("Maximum Number of Images is 2 ")
+            filelist = [f for f in os.listdir('uploads/')]
+            for f in filelist:
+                os.remove(os.path.join('uploads/', f))
+            exit()
+
+
+        names = [x[:-4] for x in myimages]
+        paths = ['uploads/' + x for x in myimages]
+        print(names) 
+    
+        descs = {}
+
+        for i in range(0,no_of_images):    
+            img_bgr = cv2.imread(paths[i])
+            image = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+            _ ,img_shapes, _ = find_faces(image,myimages[i])
+            descs[i] = encode_faces(image, img_shapes)[0]
+        np.save('train/descs.npy', descs)
+
+        descs = np.load('train/descs.npy',allow_pickle=True)[()]
+        filelist2 = [f for f in os.listdir('video/')]
+        for f in filelist2:
+            os.remove(os.path.join('video/', f))
+
+        videos = request.files.getlist("videos")
+        for f in videos:
+            if len(videos) == 1 :
+                filename = secure_filename(f.filename)
+                f.save(os.path.join(app.config['UPLOAD_VIDEO'], filename))
+                video_path = 'video/'+ f.filename
+                print(video_path)
+                cap = cv2.VideoCapture(video_path)
+                if not cap.isOpened():
+                    exit()
+        
+                _, img_bgr = cap.read()
+                padding_size = 0
+                resized_width = 1920
+                video_size = (resized_width, int(img_bgr.shape[0] * resized_width // img_bgr.shape[1]))
+                output_size = (resized_width, int(img_bgr.shape[0] * resized_width // img_bgr.shape[1] + padding_size * 2))
+
+                fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+                writer = cv2.VideoWriter('%s_output.mp4' % (video_path.split('.')[0]), fourcc, cap.get(cv2.CAP_PROP_FPS), output_size)
+
+                while True:
+                    ret, img_bgr = cap.read()
+                    if not ret:
+                        break
+
+                    img_bgr = cv2.resize(img_bgr, video_size)
+                    img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+
+                    # img_bgr = cv2.copyMakeBorder(img_bgr, top=padding_size, bottom=padding_size, left=0, right=0, borderType=cv2.BORDER_CONSTANT, value=(0,0,0))
+  
+                    dets = detector(img_bgr, 1)
+
+                    for k, d in enumerate(dets):
+                        shape = sp(img_rgb, d)
+                        face_descriptor = facerec.compute_face_descriptor(img_rgb, shape)
+
+                        last_found = {'name': 'unknown', 'dist': 0.45, 'color': (0,0,255)}
+
+                        for name, saved_desc in descs.items():
+                            dist = np.linalg.norm([face_descriptor] - saved_desc, axis=1)
+
+                            if dist < last_found['dist']:
+                                last_found = {'name': "Target", 'dist': dist, 'color': (255,255,255)}
+
+                        cv2.rectangle(img_bgr, pt1=(d.left(), d.top()), pt2=(d.right(), d.bottom()), color=last_found['color'], thickness=2)
+                        cv2.putText(img_bgr, last_found['name'], org=(d.left(), d.top()), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=last_found['color'], thickness=2)
+
+                    writer.write(img_bgr)
+                    
+                break
+                cap.release()
+                writer.release()
+
+
+
+
+
+
+
+        
+        
+
+
+
+        return render_template('current.html')
+
+    
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+if(__name__ == "__main__"):
+    app.run(debug=True)
---
+++ static/js/current.js
@@ -0,0 +1,187 @@
+$(function () {
+  $("#fileupload").change(function () {
+      if (typeof (FileReader) != "undefined") {
+          var dvPreview = $("#dvPreview");
+          dvPreview.html("");
+          var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.jpg|.jpeg|.gif|.png|.bmp)$/;
+          $($(this)[0].files).each(function () {
+              var file = $(this);
+              if (regex.test(file[0].name.toLowerCase())) {
+                  var reader = new FileReader();
+                  reader.onload = function (e) {
+                      var img = $("<img />");
+                      img.attr("class", "img-fluid img-thumbnail");
+                      img.attr("style", "height:100px;width: 100px; margin:10px");
+                      img.attr("src", e.target.result);
+                      dvPreview.append(img);
+                  }
+                  reader.readAsDataURL(file[0]);
+              } else {
+                  alert(file[0].name + " is not a valid image file.");
+                  dvPreview.html("");
+                  return false;
+              }
+          });
+      } else {
+          alert("This browser does not support HTML5 FileReader.");
+      }
+  });
+});
+  
+function resetFile() {
+  const file1 = document.getElementById("fileupload");
+  const file2 = document.getElementById("dvPreview");
+  file1.value = '';
+  file2.value = '';
+  $("#dvPreview").hide();
+}
+  
+
+
+
+function resetvideo() {
+  const file1 = document.getElementById("videobutton");
+  file1.value = '';
+  $("#up1").show();
+  $("#up2").show();
+  $("#video").hide();
+  $("#third").hide();
+}
+
+function resetthird() {
+  const file1 = document.getElementById("firstList");
+  file1.value = '';
+  $("#up1").show();
+  $("#up2").show();
+  $("#third").hide();
+  $("#mass").hide();
+}
+
+
+
+
+
+
+
+
+var htm = '';
+
+function video()
+{
+
+htm = `
+<div class="col-lg-12" style="margin-bottom: 10px;">
+<div class=" col-xs-12 col-sm-6 col-md-9">
+<div class="form-group" style = "margin-bottom: 10px;"><br>
+<div style="margin: 20px;"><b>Upload Video</b></div>
+					<div class="col-xs-12 col-sm-6 col-md-9">
+					<input type="file" class="input-file" name="videos" accept = ".mp4" multiple required/>
+					</div>
+          <button type="submit" class="btn btn-primary pull-left" style="max-width: 200px;margin-left: 10px; background-color:#2dc997; margin-bottom: 10px;"> Upload All</button>
+          <button type="button" class="btn btn-danger pull-left" style="max-width: 200px;margin-left: 10px; margin-bottom: 10px;" onclick="resetvideo()"> Reset The Field
+          </button><br><br><br>
+          </div>
+</div>
+</div>
+`
+document.getElementById("video").innerHTML= htm; 
+
+}
+
+
+
+
+
+
+
+
+function getauth(){
+
+var list1 = document.getElementById('firstList').value;
+var list2 = document.getElementById("secondList");
+
+
+if (list1 =='1')
+{
+
+list2.options.length=0;
+list2.options[0] = new Option('--Select--', '');
+list2.options[1] = new Option('Ernakulam Junction South (ERS)', '1');
+list2.options[2] = new Option('Thiruvananthapuram Central Station (TVC)', '2');
+list2.options[3] = new Option('Ernakulam Town Station (ERN)', '3');
+list2.options[4] = new Option('Angamaly Station (AFK)', '4');
+list2.options[5] = new Option('Chalakudi Railway Station (CKI)', '5');
+
+
+}
+else if (list1 =='2')
+{
+
+list2.options.length=0;
+list2.options[0] = new Option('--Select--', '');
+list2.options[1] = new Option('Police Stations-THiruvanthapuram','');
+list2.options[2] = new Option('kollam', '');
+list2.options[3] = new Option('Pathanamthitta', '');
+list2.options[4] = new Option('alappuzha', '');
+list2.options[5] = new Option('Kottayam', '');
+
+}
+}
+
+
+
+
+function third()
+{
+htm = `
+
+<centre>
+<div class="col-md-4" id ="mass">
+<h4>Select Department</h4>
+<div class ="container-fluid">
+<select class="form-control"  id='firstList' name='firstList' onclick="getauth()" required>
+<option class="dropdown-item" value="0">--Select--</option>
+						  <option class="dropdown-item" value="1">Railways</option>
+						  <option class="dropdown-item" value="2">Police</option>
+						  
+</select>
+ 
+<h4>Locate here</h4>
+<select class="form-control"  id='secondList' name='secondList' required >
+</select>
+</div><br><br>
+
+<div class ="row col-md-4 col-xs-12 col-sm-6 col-md-9"style="margin-left:5px;">
+<button type="button" class="btn btn-primary pull-left" style="max-width: 200px;margin-left: 10px; background-color:#2dc997; margin-bottom: 10px;"> Request for Video</button>
+<button type="button" class="btn btn-danger pull-left" style="max-width: 200px;margin-left: 10px; margin-bottom: 10px;" onclick="resetthird()"> Reset The Field
+          </button>
+        </div>
+ <br><br><br>
+</div>
+
+<br><br>
+</centre>
+
+
+
+`
+document.getElementById("third").innerHTML= htm;
+
+}
+
+$("#up1").click(function () {
+
+	$("#up2").hide();
+  $("#third").hide();
+  $("#video").show();
+});
+
+$("#up2").click(function () {
+
+	$("#up1").hide();
+  $("#video").hide();
+  $("#third").show();
+
+	
+
+});
